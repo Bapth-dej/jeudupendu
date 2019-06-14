6e5214c5a12253277c478d1c225f1ce1d5bfbd9f
Bapth-dej <cpmxp3vb>
2019-06-14 10:41:28 +0200

    game finished does not handle keyboard input

--- src/KeyboardLetter.js
+++ src/KeyboardLetter.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types'
 import React from 'react'
 
 const KeyboardLetter = ({letter, index, feedback, onClick}) => (
-    <span className={`letter${feedback}`} onClick={() => onClick(letter)} >
+    <span className={`letter${feedback}`} onClick={() => onClick(letter)} key={index}>
         <span className="symbol">
             {letter}
         </span>
