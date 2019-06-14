5f6c2d07c9ba4bf6d38c08e78c920fbe5d0f0ff9
Bapth-dej <cpmxp3vb>
2019-06-14 16:46:17 +0200

    should worl

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React, { Component } from 'react'
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 import KeyboardLetter from "./KeyboardLetter";
 
-const EXPRESSIONS_ARRAY = ["expression à deviner", "chocolat", "aléatoire", "bout de pain", "bouteille", "savon", "world wide web"].map(expression => (
-    expression.normalize('NFD').replace(/[\u0300-\u036f]/g, "").toUpperCase().split("")
-));
+const EXPRESSIONS_ARRAY = [
+  "expression à deviner",
+  "chocolat",
+  "aléatoire",
+  "bout de pain",
+  "bouteille",
+  "savon",
+  "world wide web"
+].map(expression =>
+  expression
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .toUpperCase()
+    .split("")
+);
 
 class App extends Component {
   state = {
@@ -12,117 +24,128 @@ class App extends Component {
     //expression à deviner
     wordArray: null,
     alphabet: "abcdefghijklmnopqrstuvwxyz".toUpperCase().split(""),
-    lettersClicked: [" "],
-  }
+    lettersClicked: [" "]
+  };
 
   // fx flechee pour le binding
   startGame = () => {
     this.setState({
       playing: true,
-      wordArray: EXPRESSIONS_ARRAY[Math.floor(Math.random() * EXPRESSIONS_ARRAY.length)],
-      lettersClicked: [" "],
-    })
-  }
+      wordArray:
+        EXPRESSIONS_ARRAY[Math.floor(Math.random() * EXPRESSIONS_ARRAY.length)],
+      lettersClicked: [" "]
+    });
+  };
 
   // fx flechee pour le binding
-  handleLetterClick = (letter) => {
-    const {lettersClicked,} = this.state
-    const letterDiscovered = lettersClicked.includes(letter)
+  handleLetterClick = letter => {
+    const { lettersClicked } = this.state;
+    const letterDiscovered = lettersClicked.includes(letter);
 
-    if ( !letterDiscovered ) {
-      this.setState(prevState => ({lettersClicked: [...lettersClicked, letter],}))
+    if (!letterDiscovered) {
+      this.setState(prevState => ({
+        lettersClicked: [...lettersClicked, letter]
+      }));
     }
+  };
+
+  wordWithHiddenLetter() {
+    const { wordArray, lettersClicked } = this.state;
+
+    return wordArray.map((letter, index) => (
+      <span className={"wordLetter"} key={index}>
+        {lettersClicked.includes(letter) ? (
+          <span className="symbol">{letter}</span>
+        ) : (
+          <span className="symbol">_</span>
+        )}
+      </span>
+    ));
   }
 
-  wordWithHiddenLetter () {
-    const {wordArray, lettersClicked} = this.state
+  getFeedbackForLetter(letter) {
+    const { lettersClicked } = this.state;
+    const letterMatched = lettersClicked.includes(letter);
 
-    return wordArray.map((letter,index) => (
-        <span className={"wordLetter"} key={index}>
-          { (lettersClicked.includes(letter)) ? <span className="symbol">{letter}</span> : <span className="symbol">_</span> }
-        </span>
-        )
-    )
-  }
-
-  getFeedbackForLetter (letter) {
-    const {lettersClicked} = this.state
-    const letterMatched = lettersClicked.includes(letter)
-
-    return letterMatched ? "clicked" : "notClicked"
+    return letterMatched ? "clicked" : "notClicked";
   }
 
   hasWon() {
-    const {wordArray, lettersClicked} = this.state
+    const { wordArray, lettersClicked } = this.state;
 
-    return !wordArray.map((letter) => (
-        lettersClicked.includes(letter)) ? letter : "_"
-    ).includes("_")
+    return !wordArray
+      .map(letter => (lettersClicked.includes(letter) ? letter : "_"))
+      .includes("_");
   }
 
   render() {
-    const {playing, wordArray, alphabet, lettersClicked,} = this.state
+    const { playing, wordArray, alphabet, lettersClicked } = this.state;
 
     return (
-        <div className="App">
-          <header className="App-header">
-            <h1>Le jeu du Pendu !</h1>
-          </header>
-
-          <div className="jeudupendu">
-
-            {playing ? (
-                <div className="playing">
-                  {this.hasWon() ? (
-                      <div className="victory">
-                        Vous avez gagné en {lettersClicked.length-1} coups
-                      </div>
-
-                  ) : (
-                      <h2>Trouvez le(s) mot(s) ci-dessous, en {wordArray.filter(function (value) {
-                        return value !== " "
-                      }).length} lettres</h2>
-                  )}
-                  <div className="wordToFind">
-                    {this.wordWithHiddenLetter(wordArray)}
-                  </div>
-
-                  {<div className="guesses">
-                    Essais ratés : {lettersClicked.map((letter, index) => (
-                          <span className="symbol" key={index}>{ (wordArray.includes(letter)) ? "" : letter}</span>
-                      )
-                  ) }
-                  </div>}
-
-                  <div className="keyboard">
-                    {this.hasWon() ? (
-                        <div className="victory">
-                          <div className="startButton" onClick={this.startGame}>
-                            <button>{"Recommencer"}</button>
-                          </div>
-                        </div>
-
-                    ) : (
-                        alphabet.map((letter, index) => (
-                            <KeyboardLetter
-                                letter={letter}
-                                index={index}
-                                key={index}
-                                feedback={this.getFeedbackForLetter(letter)}
-                                onClick={this.handleLetterClick}
-                            />
-                        ))
-                    )}
-                  </div>
+      <div className="App">
+        <header className="App-header">
+          <h1> Le jeu du Pendu! </h1>
+        </header>
+        <div className="jeudupendu">
+          {playing ? (
+            <div className="playing">
+              {this.hasWon() ? (
+                <div className="victory">
+                  Vous avez gagné en {lettersClicked.length - 1}
+                  coups
                 </div>
-            ) : (
-                <div className="startButton" onClick={this.startGame}>
-                  <button>{"Commencer"}</button>
+              ) : (
+                <h2>
+                  Trouvez le(s) mot(s) ci - dessous, en
+                  {
+                    wordArray.filter(function(value) {
+                      return value !== " ";
+                    }).length
+                  }
+                  lettres
+                </h2>
+              )}
+              <div className="wordToFind">
+                {this.wordWithHiddenLetter(wordArray)}
+              </div>
+              {
+                <div className="guesses">
+                  Essais ratés:
+                  {lettersClicked.map((letter, index) => (
+                    <span className="symbol" key={index}>
+                      {wordArray.includes(letter) ? "" : letter}
+                    </span>
+                  ))}
                 </div>
-            )}
-          </div>
+              }
+              <div className="keyboard">
+                {this.hasWon() ? (
+                  <div className="victory">
+                    <div className="startButton" onClick={this.startGame}>
+                      <button> {"Recommencer"} </button>
+                    </div>
+                  </div>
+                ) : (
+                  alphabet.map((letter, index) => (
+                    <KeyboardLetter
+                      letter={letter}
+                      index={index}
+                      key={index}
+                      feedback={this.getFeedbackForLetter(letter)}
+                      onClick={this.handleLetterClick}
+                    />
+                  ))
+                )}
+              </div>
+            </div>
+          ) : (
+            <div className="startButton" onClick={this.startGame}>
+              <button> {"Commencer"} </button>
+            </div>
+          )}
         </div>
-    )
+      </div>
+    );
   }
 }
 
--- src/KeyboardLetter.js
+++ src/KeyboardLetter.js
@@ -1,22 +1,21 @@
-import PropTypes from 'prop-types'
-import React from 'react'
+import PropTypes from "prop-types";
+import React from "react";
 
-const KeyboardLetter = ({letter, index, feedback, onClick}) => (
-    <span className={`letter${feedback}`} onClick={() => onClick(letter)} key={index}>
-        <span className="symbol">
-            {letter}
-        </span>
-    </span>
+const KeyboardLetter = ({ letter, index, feedback, onClick }) => (
+  <span
+    className={`letter${feedback}`}
+    onClick={() => onClick(letter)}
+    key={index}
+  >
+    <span className="symbol"> {letter} </span>
+  </span>
 );
 
 KeyboardLetter.propTypes = {
-    letter: PropTypes.string.isRequired,
-    feedback: PropTypes.oneOf([
-        "clicked",
-        "notClicked",
-    ]).isRequired,
-    index: PropTypes.number.isRequired,
-    onClick: PropTypes.func.isRequired,
-}
+  letter: PropTypes.string.isRequired,
+  feedback: PropTypes.oneOf(["clicked", "notClicked"]).isRequired,
+  index: PropTypes.number.isRequired,
+  onClick: PropTypes.func.isRequired
+};
 
-export default KeyboardLetter
+export default KeyboardLetter;
